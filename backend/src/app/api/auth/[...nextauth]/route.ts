77b528b3bd562b8b9a21199e8d6f546df43400c6
Katarina Vucic <kavucic@icloud.com>
2025-02-22 13:40:22 -0500

    basic auth

---
+++ backend/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+// prisma
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from '../../../../../prisma-client';
+
+
+export const authOptions = {
+    secret: process.env.NEXTAUTH_SECRET,
+  
+    // sign  in with google
+    providers: [
+        GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      }),
+
+    ],
+  
+    // prisma adapter
+    adapter: PrismaAdapter(prisma),
+  
+    // next-auth db
+    database: process.env.DATABASE_URL,
+  }
+  
+  export default NextAuth(authOptions)
