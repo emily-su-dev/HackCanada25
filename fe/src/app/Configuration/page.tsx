66bc42adfce8259db90147671e23350e5592b7c3
Katarina Vucic <kavucic@icloud.com>
2025-02-22 20:23:50 -0500

    add InfoBox

--- fe/src/app/Configuration/page.tsx
+++ fe/src/app/Configuration/page.tsx
@@ -1,8 +1,9 @@
-"use client";
+'use client';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect, useState } from "react";
-import styles from "./Configuration.module.css"; // Make sure to create this file
+import { useEffect, useState } from 'react';
+import styles from './Configuration.module.css'; // Make sure to create this file
+import InfoBox from '../components/InfoBox'; // Import the InfoBox component
 
 const Configuration = () => {
   const [mounted, setMounted] = useState(false);
@@ -17,10 +18,9 @@ const Configuration = () => {
 
   return (
     <div className={styles.configurationPage}>
-      <div className = {styles.configurationTitle}>
-            <h1>Configuration</h1>
-        </div>
-
+      <div className={styles.configurationTitle}>
+        <InfoBox />
+      </div>
     </div>
   );
 };
