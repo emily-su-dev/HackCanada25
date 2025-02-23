a3efc73280b85184bb15406a685369611998270e
Emily <emilysucanada@gmail.com>
2025-02-22 23:55:26 -0500

    change sign in, edit home page

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -5,11 +5,14 @@ import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { Button, Container } from "react-bootstrap";
 import styles from "./Home.module.css";
+import { AuthenticateButton } from './components/AuthenticateButton';
+import Image from "next/image";
 
 const Home = () => {
   const router = useRouter();
   const pathname = usePathname();
   const [mounted, setMounted] = useState(false);
+  const [moved, setMoved] = useState(false);
 
   // Fix hydration error by ensuring this runs only on the client
   useEffect(() => {
@@ -19,38 +22,39 @@ const Home = () => {
   // Prevent rendering mismatched HTML between server and client
   if (!mounted) return null;  // Ensure that the page is only rendered on the client
 
-  const handleCreateAccountClick = () => {
-    router.push("/SignUp");  // Navigate to the signup page
-  };
-
-  const handleSignInClick = () => {
-    router.push("/SignIn");
-  };
-
   return (
     <div className={styles.home}>
       <Container className="text-center">
-        <h1 className={styles.homeTitle}>Cybersecurity Awareness Training</h1>
+        <div className={styles.logoContainer}>
+          <Image
+            src="/assets/sinkerLogo.png"
+            alt="Sinker Logo"
+            className={`${styles.logo} ${moved ? styles.movedUp : ""}`} 
+            onClick={() => setMoved(!moved)}
+            width={150}
+            height={150}
+          /> 
+        </div>
+
+        <h1 
+        className={`${styles.sinkerTitle} ${moved ? styles.movedUp : ""}`} // Toggle class
+        onClick={() => setMoved(!moved)}>
+          Sinker
+        </h1>
+
+        <h1 
+        className={styles.homeTitle}>
+          Cybersecurity Awareness Training
+        </h1>
         <p className={styles.homeSubtitle}>
           Prevent phishing for once and all.
         </p>
-        <Button
-          variant="primary"
-          size="lg"
-          className={styles.createAccountBtn}
-          onClick={handleCreateAccountClick} // Handle navigation
-        >
-          Create Account
-        </Button>
-        <p className={styles.signinText + " mt-3"}>
-          Have an account?{" "}
-          <span className={styles.signinLink} onClick={() => router.push("/SignIn")}>
-            Sign in
-          </span>
-        </p>
+        
+        <AuthenticateButton />
       </Container>
     </div>
   );
 };
 
 export default Home;
+
